98cabdb0fdc83fa5a9c829d419d49803368cb8f2
WeiXi <sunshiwei418@126.com>
2025-02-10 00:13:09 +0800

    feat: add xcode project

---
+++ EchoPulse/EchoPulse/EchoPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoPulseApp.swift
+//  EchoPulse
+//
+//  Created by 孙世伟 on 2025/2/10.
+//
+
+import SwiftUI
+
+@main
+struct EchoPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EchoPulse/EchoPulseTests/EchoPulseTests.swift
@@ -0,0 +1,17 @@
+//
+//  EchoPulseTests.swift
+//  EchoPulseTests
+//
+//  Created by 孙世伟 on 2025/2/10.
+//
+
+import Testing
+@testable import EchoPulse
+
+struct EchoPulseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
