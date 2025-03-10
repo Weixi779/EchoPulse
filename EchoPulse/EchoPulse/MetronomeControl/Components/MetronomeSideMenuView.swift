22c04e05ea9188c4795e1ae94d88a5129e87c710
WeiXi <sunshiwei418@126.com>
2025-03-10 15:15:36 +0800

    feat: scrollerView first version

--- EchoPulse/EchoPulse/MetronomeControl/Components/MetronomeSideMenuView.swift
+++ EchoPulse/EchoPulse/MetronomeControl/Components/MetronomeSideMenuView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct MetronomeSideMenuView: View {
     @Binding var isShowMenu: Bool
-//    @Binding var selectedSound: MetronomeSourceType
+    @Binding var selectedSound: MetronomeSourceType
+    
+    private let cellHeight: CGFloat = 150
+    private let spacing: CGFloat = 50
+    
+    private let sources = MetronomeSourceType.allCases
     
     var body: some View {
         ZStack {
@@ -31,14 +36,51 @@ struct MetronomeSideMenuView: View {
                             .foregroundStyle(.white)
                             .ignoresSafeArea()
                         
-                        VStack {
-                            ScrollView {
-                                ForEach(MetronomeSourceType.allCases) { source in
-                                    Text(source.fileName)
+                        GeometryReader { geo in
+                            ScrollViewReader { proxy in
+                                ScrollView {
+                                    LazyVStack(spacing: spacing) {
+                                        ForEach(sources) { source in
+                                            Text(source.fileName)
+                                                .font(.largeTitle)
+                                                .frame(height: cellHeight)
+                                                .background(Color.gray)
+                                                .scrollTargetLayout()
+                                                .scrollTransition(
+                                                    axis: .vertical
+                                                ) { content, phase in
+                                                    content
+                                                        .rotationEffect(.degrees(phase.value * -10), anchor: .bottomTrailing)
+                                                        .offset(x: phase.isIdentity ? 0 : 15)
+                                                        .opacity(phase.isIdentity ? 1 : 0)
+                                                }
+                                        }
+                                    }
+                                    .padding(.vertical, (geo.size.height - cellHeight) / 2)
+                                }
+                                .onScrollGeometryChange(for: CGPoint.self) { geometry in
+                                    geometry.contentOffset
+                                } action: { oldOffset, newOffset in
+                                    let baseOffset = geo.safeAreaInsets.top
+                                    let totalHeight = cellHeight + spacing
+                                    
+                                    let index = Int(round((newOffset.y + (totalHeight / 2) - baseOffset) / totalHeight))
+                                    
+                                    if index >= 0 && index < sources.count {
+                                        let newItem = sources[index]
+                                        if selectedSound != newItem {
+                                            selectedSound = newItem
+                                        }
+                                    }
+                                }
+                                .onScrollPhaseChange { oldPhase, newPhase in
+                                    if newPhase == .idle {
+                                        withAnimation(.easeInOut) {
+                                            proxy.scrollTo(selectedSound, anchor: .center)
+                                        }
+                                    }
                                 }
                             }
-                            
-                            Spacer()
                         }
                     }
                     .frame(maxWidth: 250)
@@ -50,5 +92,15 @@ struct MetronomeSideMenuView: View {
 }
 
 #Preview {
-    MetronomeSideMenuView(isShowMenu: .constant(true))
+    struct PreviewWrapper: View {
+        @State var selectedSound: MetronomeSourceType = .bassDrum
+        
+        var body: some View {
+            MetronomeSideMenuView(
+                isShowMenu: .constant(true),
+                selectedSound: $selectedSound
+            )
+        }
+    }
+    return PreviewWrapper()
 }
--- EchoPulse/EchoPulse/MetronomeControl/MetronomeControlView.swift
+++ EchoPulse/EchoPulse/MetronomeControl/MetronomeControlView.swift
@@ -37,18 +37,18 @@ struct MetronomeControlView: View {
                     backgroundColor: .blue,
                     cornerRadius: 20
                 )
-
+                
                 MetronomeControlSlider(title: "BPM", value: $viewModel.bpm, range: 40...240, step: 1) {
                     viewModel.updateBPM(viewModel.bpm)
                 }
-
+                
                 MetronomeControlSlider(title: "Volume", value: $viewModel.volume, range: 0.0...1.0, step: 0.1, multiplier: 100, unit: "%") {
                     viewModel.updateVolume(viewModel.volume)
                 }
             }
             .padding()
             
-            MetronomeSideMenuView(isShowMenu: $isShowMenu)
+            MetronomeSideMenuView(isShowMenu: $isShowMenu, selectedSound: $viewModel.sourceType)
         }
     }
 }
